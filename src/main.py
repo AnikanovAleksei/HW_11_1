653e6e0c4665de7fd2fd917797514c739ef393a5
Aleksei Anikanov <python_work@bk.ru>
2024-07-03 12:09:38 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import src.widget
+
+print(src.widget.mask_account_card("Visa Platinum 8990922113665229"))
+print(src.widget.mask_account_card("MasterCard 7158300734726758"))
+print(src.widget.mask_account_card("Visa Classic 6831982476737658"))
+print(src.widget.get_date("2018-07-11T02:26:18.671407"))
